418ae855a13a7cf19bd9fd407a87e2317c6bfde5
Joyboy04 <raihanrevikasya12@gmail.com>
2025-06-06 23:17:43 +0700

    Docker Compose

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -6,11 +6,18 @@ export const startServer = () => {
     const httpServer = express();
     const port = config.port;
 
+    httpServer.get('/ping', (req, res) => {
+        console.log('-ping route:', req.url, Date.now());
+        res.status(200).json({
+            Message: 'pong - test successful',
+        })
+    })
+
     try {
         httpServer.listen(port, () => {
             console.log('server started on port', port);
         });
     } catch(err) {
-        throw new Error(`Error starting server: ${err}`);
+        throw new Error('Error starting server: ${err}');
     }
 }
